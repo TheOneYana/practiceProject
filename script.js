a84dd55a9d71cd54dba794279b0c81ecf3b33aa8
TheOneYana <yanatheone@yahoo.com>
2023-03-23 21:10:49 -0700

    test

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ btn.addEventListener('click', function(){
     btn.style.backgroundColor === 'bisque'? btn.style.backgroundColor = 'rgb(12, 113, 186)' : btn.style.backgroundColor = 'bisque';
     btn.style.backgroundColor === 'bisque'? btn.style.color = 'green' : btn.style.color = 'bisque';
 
-})
+});
