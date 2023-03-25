fc2a3b3aeaf325f2217baae65967f3d0120d30b2
Uliana Sarkisova <yanatheone@Ulianas-MacBook-Air.local>
2023-03-24 19:03:11 -0700

    css and js files updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById("btn");
+const loginBtn = document.getElementById("loginBtn");
 
 btn.addEventListener("click", function () {
   btn.style.backgroundColor === "bisque"
@@ -7,4 +8,12 @@ btn.addEventListener("click", function () {
   btn.style.backgroundColor === "bisque"
     ? (btn.style.color = "green")
     : (btn.style.color = "bisque");
+});
+loginBtn.addEventListener("click", function () {
+  loginBtn.style.backgroundColor === "rgb(245, 237, 4)"
+    ? (loginBtn.style.backgroundColor = "crimson")
+    : (loginBtn.style.backgroundColor = "rgb(245, 237, 4)");
+  loginBtn.style.color === "green"
+    ? (loginBtn.style.color = "white")
+    : (loginBtn.style.color = "green");
 });
