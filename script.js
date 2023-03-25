73c9366e26433bbca91698464ded0416c4690990
Uliana Sarkisova <yanatheone@Ulianas-MacBook-Air.local>
2023-03-24 18:51:02 -0700

    css updated

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
-btn.addEventListener('click', function(){
-    btn.style.backgroundColor === 'bisque'? btn.style.backgroundColor = 'rgb(12, 113, 186)' : btn.style.backgroundColor = 'bisque';
-    btn.style.backgroundColor === 'bisque'? btn.style.color = 'green' : btn.style.color = 'bisque';
-
+btn.addEventListener("click", function () {
+  btn.style.backgroundColor === "bisque"
+    ? (btn.style.backgroundColor = "rgb(12, 113, 186)")
+    : (btn.style.backgroundColor = "bisque");
+  btn.style.backgroundColor === "bisque"
+    ? (btn.style.color = "green")
+    : (btn.style.color = "bisque");
 });
